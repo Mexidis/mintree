96c5a5b26240764f5a40c8565e7606185bdd17bc
Mexidis <mexidismartinez@gmail.com>
2025-10-27 01:51:00 -0600

    ABC Heuristic, created, needs logic to be implemented

--- app/src/main/kotlin/hoc/unam/ABC.kt
+++ app/src/main/kotlin/hoc/unam/ABC.kt
@@ -1,5 +1,14 @@
 package hoc.unam
 
-class ABC {
-    val hive: List<Bee>
+class ABC (
+    private val graph: Graph,
+    private val k: Int,
+
+    private val SN: Int,  // population size
+    private val MCN: Int, // maximun cycle number
+    private val limit: Int
+){
+    private val hive = Hive(SN)
+    private val restrictionManager: Nothing = TODO()
+    private lateinit var bestGlobalSolution: Solution
 }
